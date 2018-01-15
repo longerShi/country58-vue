9449071c724973690371bb3885d0cee61928e79b
史文龙 <shiwenlong@shiwenlongdeMacBook-Pro.local>
2018-01-15 10:11:10 +0800

    add promise

--- src/page/home/home.vue
+++ src/page/home/home.vue
@@ -8,12 +8,13 @@
         </grid-item>
       </grid>
     </div>
-    <panel :header="header" :list="list" :type="type"></panel>
+    <panel  v-if="list" :header="header" :list="list" :type="type"></panel>
   </div>
 </template>
 
 <script>
   import {Grid, GridItem, GroupTitle, Panel} from 'vux'
+  import Api from '../server/api'
 
   export default {
     name: 'home',
@@ -25,63 +26,43 @@
     },
     data () {
       return {
-        header: 'header',
-//        footer: {url: 'url', title: 'title'},
-        list: [{
-          src: 'http://placeholder.qiniudn.com/60x60/3cc51f/ffffff',
-          title: '标题一',
-          desc: '由各种物质组成的巨型球状天体，叫做星球。星球有一定的形状，有自己的运行轨道。',
-          url: {
-            path: '/component/radio',
-            replace: false
-          },
-          meta: {
-            source: '来源信息',
-            date: '时间',
-            other: '其他信息'
-          }
-        }, {
-          src: 'http://placeholder.qiniudn.com/60x60/3cc51f/ffffff',
-          title: '标题二',
-          desc: '由各种物质组成的巨型球状天体，叫做星球。星球有一定的形状，有自己的运行轨道。',
-          url: {
-            path: '/component/radio',
-            replace: false
-          },
-          meta: {
-            source: '来源信息',
-            date: '时间',
-            other: '其他信息'
-          }
-        }],
+        header: 'tech list',
+        list: [],
         type: '5'
       }
+    },
+
+    created() {
+      var self = this;
+      Api.getTechList().then(
+        function(value) {
+          console.log(value)
+          value.techs.forEach(function(data) {
+            self.list.push({
+              src: data.headImg,
+              title: data.techName,
+              desc: data.address,
+              url: {
+                path: '/component/radio',
+                replace: false
+              },
+              meta: {
+                // source: '来源信息',
+                date: data.phone,
+                other: data.techType
+              }
+            });
+          });
+        }, function(error) {
+          // failure
+        });
     }
+
   }
 </script>
 
 
 <style>
-  .icon {
-    display: inline-block;
-    width: 28px;
-    height: 28px;
-    vertical-align: middle;
-  }
 
-  .icon_button {
-    background: url(../../assets/images/icon_nav_button.png) no-repeat;
-    background-size: 28px 28px;
-  }
-
-  .icon_cell {
-    background: url(../../assets/images/icon_nav_cell.png) no-repeat;
-    background-size: 28px 28px;
-  }
-
-  .icon_toast {
-    background: url(../../assets/images/icon_nav_toast.png) no-repeat;
-    background-size: 28px 28px;
-  }
 </style>
 
---
+++ src/page/server/api.js
@@ -0,0 +1,41 @@
+import axios from 'axios'
+
+axios.defaults.timeout = 5000;
+axios.defaults.headers.post['Content-Type'] = 'application/json;charset=UTF-8';
+axios.defaults.baseURL = 'http://localhost:8081/';
+
+
+function postRequest(url, params) {
+  return new Promise((resolve, reject) => {
+    axios.post(url, params)
+      .then(response => {
+        resolve(response.data);
+      }, err => {
+        reject(err);
+      })
+      .catch((error) => {
+        reject(error)
+      })
+  })
+}
+
+export function getRequest(url, params) {
+  return new Promise((resolve, reject) => {
+    axios.get(url, {params: params})
+      .then(response => {
+        resolve(response.data);
+      }, err => {
+        reject(err);
+      })
+      .catch((error) => {
+        reject(error)
+      })
+  })
+}
+
+
+export default {
+  getTechList() {
+    return getRequest('/techs', {});
+  }
+};
